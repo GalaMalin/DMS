df5b426eef44e7ead39a82c7fb72ac7d2cd0a69b
GalaMalin <126569341+GalaMalin@users.noreply.github.com>
2024-12-03 00:22:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
+// При нажатии на кнопку появляется блок с карточкой
+// Блоки кнопок
+const center_btn_z = document.getElementById("center_btn-zz")
+const center_btn_o = document.getElementById("center_btn-oo")
+const center_btn_r = document.getElementById("center_btn-rr")
+const center_btn_s = document.getElementById("center_btn-ss")
 
-function Calendar2(id, year, month) {
-    var Dlast = new Date(year,month+1,0).getDate(),
-        D = new Date(year,month,Dlast),
-        DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
-        DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
-        calendar = '<tr>',
-        month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-    if (DNfirst != 0) {
-      for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
-    }else{
-      for(var  i = 0; i < 6; i++) calendar += '<td>';
-    }
-    for(var  i = 1; i <= Dlast; i++) {
-      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-        calendar += '<td class="today">' + i;
-      }else{
-        calendar += '<td>' + i;
-      }
-      if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
-        calendar += '<tr>';
-      }
-    }
-    for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-    document.querySelector('#'+id+' tbody').innerHTML = calendar;
-    document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
-    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
-    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
-    if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-        document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
-    }
-    }
-    Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
-    // переключатель минус месяц
-    document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-      Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
-    }
-    // переключатель плюс месяц
-    document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-      Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
-    }
+// Блоки карточек
+let div_z = document.querySelector('.card-z');
+let div_o = document.querySelector('.card-o');
+let div_r = document.querySelector('.card-r');
+let div_s = document.querySelector('.card-s');
+
+// Функция, меняющая видимость блока с карточкой
+center_btn_z.addEventListener('click', () => {
+    div_z.style.display = getComputedStyle(div_z).display == 'block' ? 'none' : 'block';     
+  });
+
+center_btn_o.addEventListener('click', () => {
+    div_o.style.display = getComputedStyle(div_o).display == 'block' ? 'none' : 'block';
+  });
 
 
-    
+center_btn_r.addEventListener('click', () => {
+    div_r.style.display = getComputedStyle(div_r).display == 'block' ? 'none' : 'block';
+  });
+
+center_btn_s.addEventListener('click', () => {
+    div_s.style.display = getComputedStyle(div_s).display == 'block' ? 'none' : 'block';
+  });
